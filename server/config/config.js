ad3e0a3974a906e2403aaddc4dbe21a5bc67c706
kevingu <kevingu2@yahoo.com>
2016-04-23 01:52:57 -0700

    change

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@
  * Config var for app
 **/
 module.exports = {
-  mongoDBUrl: process.env.MONGOLAB_URI || process.env.MONGODB_URL || 'mongodb://localhost:27017/plug',
+  mongoDBUrl: process.env.MONGODB_URL || process.env.MONGOLAB_URI  || 'mongodb://localhost:27017/plug',
   port: process.env.PORT || 3000,
   secret: process.env.SECRET || 'mysecret'
 };
